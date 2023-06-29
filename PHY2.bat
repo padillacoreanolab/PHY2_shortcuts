30c2001e07c82a0649ab3ba8b578827db69feab2
padillacoreanolab <padillacoreanolab@gmail.com>
2023-06-29 11:24:16 -0400

    working

--- PHY2.bat
+++ PHY2.bat
@@ -1,7 +1,6 @@
 @echo off
 echo Starting PHY2...
-%windir%\System32\cmd.exe "/C" C:\ProgramData\anaconda3\Scripts\activate.bat phy2 
-echo phy2 environment activated...
+call C:\ProgramData\anaconda3\Scripts\activate.bat phy2 
+echo PHY2 environment activated...
 @SET /p param_path=Enter Params.py file path:
-START "" /min cmd.exe /c "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Anaconda3 (64-bit)\Anaconda Prompt (anaconda3).lnk"
-%windir%\System32\cmd.exe "/K" phy template-gui %param_path%/params.py
+@CALL phy template-gui %param_path%\params.py
