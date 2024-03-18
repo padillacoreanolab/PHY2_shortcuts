1697fd2273e36fa3c09c9d92347ccad1cb71b255
padillacoreanolab <padillacoreanolab@gmail.com>
2024-03-18 13:51:01 -0400

    feat: plugins

---
+++ plugins/2ms_isi_violations.py
@@ -0,0 +1,24 @@
+# import from plugins/cluster_metrics.py
+"""Show how to add a custom cluster metrics."""
+
+import numpy as np
+from phy import IPlugin
+
+
+class ISIViolationPlugin2ms(IPlugin):
+    def attach_to_controller(self, controller):
+        """Note that this function is called at initialization time, *before* the supervisor is
+        created. The `controller.cluster_metrics` items are then passed to the supervisor when
+        constructing it."""
+
+        def isi_violations(cluster_id):
+            t = controller.get_spike_times(cluster_id).data
+
+            all_isi = np.diff(t)
+            return len(all_isi[all_isi < 0.0021])
+
+        # Use this dictionary to define custom cluster metrics.
+        # We memcache the function so that cluster metrics are only computed once and saved
+        # within the session, and also between sessions (the memcached values are also saved
+        # on disk).
+        controller.cluster_metrics['2ms_isi'] = controller.context.memcache(isi_violations)
